3398d7d1bf9fd3ebb55c956e610748058a29924f
YejiJeonn <110978687+YejiJeonn@users.noreply.github.com>
2023-05-24 01:52:15 +0900

    Delete HBD directory

--- HBD/funtion.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('Birthday').value = new Date().toISOString().substring(0, 10);
-
-const fileInput = document.getElementById("fileup");
-// 또는 const fileInput = $("#fileUpload").get(0);
-
-const handleFiles = () => {
-  const selectedFile = [...fileInput.files];
-  console.log(selectedFile);
-};
